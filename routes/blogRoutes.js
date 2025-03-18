d6c656bb4a390d18990c2fa532cc7dbb491c1757
devSereneMINDS <serenemindsdevteam@gmail.com>
2025-03-18 22:51:22 +0530

    Update blogRoutes.js

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -4,13 +4,15 @@ import {
     getBlogById,
     createBlog,
     updateBlog,
-    deleteBlog
+    deleteBlog,
+    getBlogBySlug
 } from "../controllers/blogController.js";
 
 const router = express.Router();
 
 router.get("/all", getAllBlogs);
 router.get("/:blogId", getBlogById);
+router.get("/slug/:slug",getBlogBySlug);
 router.post("/create", createBlog);
 router.put("/:blogId", updateBlog);
 router.delete("/:blogId", deleteBlog);
