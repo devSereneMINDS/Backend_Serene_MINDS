4f9403aab4b9f585f6899e383a26731667632477
devSereneMINDS <serenemindsdevteam@gmail.com>
2025-06-09 22:57:54 +0530

    Update sendRoutes.js

--- routes/sendRoutes.js
+++ routes/sendRoutes.js
@@ -1,9 +1,11 @@
 import express from "express";
-import { sendInvitation } from "../controllers/sendEmail.js";  // Import the controller
+import { sendInvitation,sendCustomEmail } from "../controllers/sendEmail.js";  // Import the controller
 
 const router = express.Router();
 
 // Route to handle sending invitations via email
 router.post("/new", sendInvitation);
+
+router.post("/custom", sendCustomEmail);
 
 export default router;
