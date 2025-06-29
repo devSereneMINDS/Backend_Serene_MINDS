a3439fa57c4b2d033c6d9fdd1b6bf3b23439e87e
devSereneMINDS <serenemindsdevteam@gmail.com>
2025-06-29 17:12:36 +0530

    Create aisensy-template.js

---
+++ controllers/aisensy-template.js
@@ -0,0 +1,3 @@
+export const PROFESSIONAL_APPOINTMENT_NOTIFICATION = "professional_appointment";
+export const CLIENT_APPOINTMENT_CONFIRMATION = "client_appointment_details03";
+export const CLIENT_ONBOARDING_WELCOME = "client_onboarding_02";
